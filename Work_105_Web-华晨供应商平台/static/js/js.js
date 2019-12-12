f5b2ae3fe025a38bd1b2b814ad4b923529bbd63b
lucciv <lucciv@163.com>
2019-12-12 16:07:15 +0800

    Work_105 - No.2-修改项目： 1. 登录页垂直居中; 2. 铺满背景图; 3. 首页的尾部置底。

--- Work_105_Web-华晨供应商平台/static/js/js.js
+++ Work_105_Web-华晨供应商平台/static/js/js.js
@@ -1,9 +1,4 @@
 $(document).ready(function () {
 	// textScroll
 	jQuery(".txtScroll-left").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"left",autoPlay:true,scroll:3,vis:4,trigger:"click"});
-	// Toggle
-	var strNum = $(".ba_wordContent h5,.ba_wordContent p").text().length;
-	if (strNum > 200) {
-	  $("#ba_togglebox").hide();
-	}
 })
